fc64c3ea6d55d812bb4f23278a3a7ffd41d38e98
Rupalib <bhattarupali2011@gmail.com>
2025-01-29 16:04:07 +0530

    remove add function

--- demo.py
+++ demo.py
@@ -1,9 +1,9 @@
 print("1st demo on git")
 print("2nd demo on git")
-def add():
-    a = 5
-    b = 10
-    return a+b
+# def add():
+#     a = 5
+#     b = 10
+#     return a+b
 def substract(a,b):
     return a-b
 # def multiply(a,b):
